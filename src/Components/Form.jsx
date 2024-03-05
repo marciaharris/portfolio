b4664b4ead36032df515cd2be9ac30b4427712ed
Marcia <hmarcia40@gmail.com>
2024-03-05 00:14:47 -0500

    CSS styling

---
+++ src/Components/Form.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+
+export function Form () {
+    const [form, setFormData] = useState({
+        name:"",
+        email: "",
+        number: "",  
+        comments:"",      
+    })
+    function handleChange(event){
+        const {value,type,name} = event.target;
+        setFormData(prevData =>({
+            ...prevData,
+            [name] : value,
+        }))
+    }
+    function onSubmit (){
+        console.log(form);
+    }
+    return (
+        <>
+            <form>
+            <input
+            placeholder="Full Name"
+            value={form.name}
+            type="text"
+            name="name"
+            onChange={handleChange}
+            />
+            <input
+            placeholder="Email"
+            value={form.email}
+            type="email"
+            name="email"
+            onChange={handleChange}
+            />
+            <input
+            placeholder="Cell Number"
+            value={form.number}
+            type="string"
+            name="number"
+            onChange={handleChange}
+            />
+            <textarea 
+                value={form.comments}
+                placeholder="Please explain your interest..."
+                onChange={handleChange}
+                name="comments"
+            />
+            <button>Submit</button>
+           </form>
+        </>
+    )
+}
