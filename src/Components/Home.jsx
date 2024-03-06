cba4d7504900e2131d493814b508f3003e473ab4
Marcia <hmarcia40@gmail.com>
2024-03-05 20:44:49 -0500

    CSS update, Home route changed to root route

--- src/Components/Home.jsx
+++
@@ -1,9 +0,0 @@
-export default function Home (){
-    return (
-        <div>
-            <h1>Hi</h1>
-            <br></br>
-            <h1>I'm Marcia, a web developer...</h1>
-        </div>
-    )
-}
